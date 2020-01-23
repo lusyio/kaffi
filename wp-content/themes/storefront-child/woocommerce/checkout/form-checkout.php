e5450e1469c329636547462ea87a0bdab1e8e5d0
mastekator <akmiskam@yandex.ru>
2020-01-23 17:28:51 +0300

    Update form-checkout.php

--- wp-content/themes/storefront-child/woocommerce/checkout/form-checkout.php
+++ wp-content/themes/storefront-child/woocommerce/checkout/form-checkout.php
@@ -46,6 +46,19 @@ if (!defined('ABSPATH')) {
                 и оплаты</h3>
 
             <?php do_action('woocommerce_checkout_before_order_review'); ?>
+            <script id="ISDEKscript" type="text/javascript" src="https://widget.cdek.ru/widget/widjet.js"></script>
+            <script type="text/javascript">
+                var ourWidjet = new ISDEKWidjet ({
+                    defaultCity: 'Москва', //какой город отображается по умолчанию
+                    cityFrom: 'Москва', // из какого города будет идти доставка
+                    country: 'Россия', // можно выбрать страну, для которой отображать список ПВЗ
+                    link: 'forpvz', // id элемента страницы, в который будет вписан виджет
+                    path: 'https://widget.cdek.ru/widget/scripts/', //директория с библиотеками виджета
+                    servicepath: 'http://localhost:8888/service.php', //ссылка на файл service.php на вашем сайте
+                    apikey: 'fcfa7a2e-3837-4c84-b783-87041d648ad5',
+                });
+            </script>
+            <div id="forpvz" style="width:100%; height:600px;"></div>
 
             <div id="order_review" class="woocommerce-checkout-review-order">
 
