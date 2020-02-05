71893acba67db24fdc47427dd4f3c705eb836617
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-02-05 18:01:29 +0300

    видео отзыв

--- wp-content/themes/storefront-child/woocommerce/single-product/tabs/description.php
+++ wp-content/themes/storefront-child/woocommerce/single-product/tabs/description.php
@@ -121,187 +121,210 @@ $tag_count = sizeof(get_the_terms($post->ID, 'product_tag'));
 $newview = get_post_meta($post->ID, 'newview', true);
 //Проверка на существование поля Songs
 if ($newview) : ?>
-    <?php
-    global $product;
-    global $post;
-    $id = $product->id;
-    if ($newview == 'white') {
-        $white = 'white';
-    }
-    echo '
+<?php
+global $product;
+global $post;
+$id = $product->id;
+if ($newview == 'white') {
+    $white = 'white';
+}
+?>
 </div>
 </div>
 </div>
 </div>
-    </div>
-        <div class="model-image-block hidden-xs" style="background-repeat: no-repeat;background-image: url(/images/model/' . $id . '.jpg)">
-            <div class="container"> 
-                <div class="row"> 
-                    <div class="col-xl-4 col-6 mt-4 ' . $white . '">
-                     <h2>' . get_post_meta($post->ID, 'short_name', true) . '</h2> 
-                     <p class="imbp">' . get_post_meta($post->ID, 'short_opisanie', true) . '</p> 
-                     <p class="mib-grey"> </p> 
-                    </div>
-                </div>
-            </div>
-        </div>
+</div>
+<div class="model-image-block hidden-xs"
+     style="background-repeat: no-repeat;background-image: url(/images/model/<?= $id ?>.jpg)">
     <div class="container">
         <div class="row">
-            <div class="col-12">
-    '; ?>
-<?php endif; ?>
-
-
-<?php
-
-$youtube = get_post_meta($post->ID, 'youtube', true);
-//Проверка на существование поля Songs
-if ($youtube) : ?>
-    <?php
-    global $product;
-    global $post;
-    $id = $product->id;
-    echo '
+            <div class="col-xl-4 col-6 mt-4 ' . $white . '">
+                <h2><?= get_post_meta($post->ID, 'short_name', true) ?></h2>
+                <p class="imbp"><?= get_post_meta($post->ID, 'short_opisanie', true) ?></p>
+                <p class="mib-grey"></p>
+            </div>
+        </div>
+    </div>
 </div>
-</div>
-</div>
-</div>
-</div>
-<div class="container pd30 videopost"><div class="row"><div class="col-sm-8"><div class="video-box"><div class="videoWrapper obzor"><iframe width="560" height="315" src="https://www.youtube.com/embed/' . get_post_meta($post->ID, 'youtube', true) . '?rel=0" frameborder="0" allowfullscreen></iframe></div></div></div><div class="col-sm-4 entry-content"><div class="pdt120"><h2>Видео-обзор на кафф<br>' . get_post_meta($post->ID, 'short_name', true) . '</h2><p>Кафф ' . get_post_meta($post->ID, 'short_name', true) . ' имеет универсальный размер. Отлично смотрится, хорошо сидит и не спадает.</p><p>Данное изделие может стать удачным подарком для себя и любимых.</p></div></div></div></div></div>
 <div class="container">
     <div class="row">
         <div class="col-12">
-'; ?>
-<?php endif; ?>
+            <?php endif;
+            $youtube = get_post_meta($post->ID, 'youtube', true);
+            if (!$youtube){
+                $otz_video = true;
+                $youtube = get_post_meta($post->ID, 'otz_video', true);
+            }
 
-<?php
-
-$youtube = get_post_meta($post->ID, 'img1', true);
-//Проверка на существование поля Songs
-if ($youtube) : ?>
-    <?php
-    global $product;
-    global $post;
-    $id = $product->id;
-    echo '
+            //Проверка на существование поля Songs
+            if ($youtube) : ?>
+            <?php
+            global $product;
+            global $post;
+            $id = $product->id;
+            ?>
+        </div>
+    </div>
 </div>
 </div>
+</div>
+<div class="container pd30 videopost">
+    <div class="row">
+        <div class="col-sm-8">
+            <div class="video-box">
+                <div class="videoWrapper obzor">
+                    <iframe width="560" height="315"
+                            src="https://www.youtube.com/embed/<?= $youtube ?>?rel=0"
+                            frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 entry-content">
+            <div class="pdt120">
+                <?php if ($otz_video): ?>
+                    <h2>Видео-отзыв на кафф<br><?= get_post_meta($post->ID, 'short_name', true) ?></h2>
+                <?php else: ?>
+                    <h2>Видео-обзор на кафф<br><?= get_post_meta($post->ID, 'short_name', true) ?></h2>
+                <?php endif; ?>
+                <p>Кафф <?= get_post_meta($post->ID, 'short_name', true) ?> имеет универсальный размер. Отлично
+                    смотрится, хорошо сидит и не спадает.</p>
+                <p>Данное изделие может стать удачным подарком для себя и любимых.</p>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <?php endif;
+            $youtube = get_post_meta($post->ID, 'img1', true);
+
+            if ($youtube) : ?>
+            <?php
+            global $product;
+            global $post;
+            $id = $product->id;
+            ?>
+        </div>
+    </div>
 </div>
 </div>
 </div>
 <div class="block-1400  hidden-xs">
     <div class="pdb30 bg-grey">
         <div class="container pd30">
-            <div >
+            <div>
                 <div class="row">
                     <div class="col-md-6 col-sm-12 entry-content">
                         <div class="product-main description-cards">
-                            <h2>Кафф ' . get_post_meta($post->ID, 'short_name', true) . '</h2>
-                            <p>' . get_post_meta($post->ID, 'desc1', true) . '</p>
+                            <h2>Кафф <?= get_post_meta($post->ID, 'short_name', true) ?></h2>
+                            <p><?= get_post_meta($post->ID, 'desc1', true) ?></p>
                         </div>
-                        
-                        <img class="mt30" src="' . get_post_meta($post->ID, 'img2', true) . '">
+
+                        <img class="mt30" src="<?= get_post_meta($post->ID, 'img2', true) ?>">
                     </div>
                     <div class="col-md-6 entry-content">
-                        <img class="hidden-sm" src="' . get_post_meta($post->ID, 'img1', true) . '">
+                        <img class="hidden-sm" src="<?= get_post_meta($post->ID, 'img1', true) ?>">
                         <div class="product-main mt30" style="padding: 20px 50px 40px;background: white;">
-                            <h2>Кафф ' . get_post_meta($post->ID, 'short_name', true) . ' - кое-что особенно из мира украшений</h2>
-                            <p>' . get_post_meta($post->ID, 'desc2', true) . '</p>
+                            <h2>Кафф <?= get_post_meta($post->ID, 'short_name', true) ?> - кое-что особенно из мира
+                                украшений</h2>
+                            <p><?= get_post_meta($post->ID, 'desc2', true) ?></p>
                         </div>
                     </div>
                 </div>
             </div>
-       
         </div>
     </div>
 </div>
 <div class="container">
     <div class="row">
         <div class="col-12">
-'; ?>
 
+            <?php else: ?>
+                <div class="block-1400">
+                    <div class="bg-grey pdb30">
+                        <div class="container pd30">
 
-<?php else: ?>
-    <div class="block-1400">
-        <div class="bg-grey pdb30">
-            <div class="container pd30">
+                            <div class="desc-product entry-content">
+                                <?php the_content(); ?>
+                            </div>
 
-                <div class="desc-product entry-content">
-                    <?php the_content(); ?>
+                        </div>
+                    </div>
                 </div>
-
-            </div>
-        </div>
-    </div>
-<?php endif; ?>
+            <?php endif; ?>
 
 
-<div class="container stepa-container mt-3">
-    <div class="row mt30">
-        <div class="col-lg-4 d-none d-lg-flex">
-            <p class="stepa-abouts">Мастер Степан Васильев – единственный в России, посвятивший себя каффам — столь
-                необычным сережкам, способным украсить не только мочку, но и все ушко...</p>
-        </div>
-        <div class="col-lg-4 col-12 text-center">
-            <a href="/stepan-vasiliev" title="Степан Васильев"><img src="/images/stepa-master.jpg"
-                                                                    alt="Степан Васильев"
-                                                                    class="stepan mt30"></a>
-            <p class="stepa-zitat">Каждый кафф создан вручную, наполнен любовью и вниманием к деталям</p>
-            <img src="/images/signature.png" alt="Степан Васильев">
-            <p class="mt30 text-center"><a href="/shop" class="button-more addtocartbutton">Посмотреть другие каффы</a>
-            </p>
-        </div>
-        <div class="col-lg-4 d-none d-lg-flex">
-            <p class="stepa-abouts">Каффы - это настоящая находка для тех, у кого не проколоты ушки, ведь большинство
-                моделей не требуют проколов вообще. Попробуйте и вы кое-что особенное...</p>
-        </div>
-    </div>
-</div>
-
-<!-- Initialize Swiper -->
-<script>
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        slidesPerView: 1,
-        paginationClickable: true,
-        spaceBetween: 30,
-        loop: true
-    });
-</script>
-
-
-<div class="block-1400">
-    <div class="bg-grey pdb30">
-        <div class="pd30 text-center komplect">
-            <div class="container">
-                <hr>
-            </div>
-            <h2>Комплект поставки</h2>
-            <div class="container ">
-
-                <div class="row">
-                    <div class="col-sm-4">
-                        <img src="/images/box.jpg">
-                        <p>Ювелирная коробочка</p>
+            <div class="container stepa-container mt-3">
+                <div class="row mt30">
+                    <div class="col-lg-4 d-none d-lg-flex">
+                        <p class="stepa-abouts">Мастер Степан Васильев – единственный в России, посвятивший себя каффам
+                            — столь
+                            необычным сережкам, способным украсить не только мочку, но и все ушко...</p>
                     </div>
-                    <div class="col-sm-4">
-                        <?php echo get_the_post_thumbnail($page->ID, 'full'); ?>
-                        <p>Кафф <?php $key = "short_name";
-                            echo get_post_meta($post->ID, $key, true); ?></p>
+                    <div class="col-lg-4 col-12 text-center">
+                        <a href="/stepan-vasiliev" title="Степан Васильев"><img src="/images/stepa-master.jpg"
+                                                                                alt="Степан Васильев"
+                                                                                class="stepan mt30"></a>
+                        <p class="stepa-zitat">Каждый кафф создан вручную, наполнен любовью и вниманием к деталям</p>
+                        <img src="/images/signature.png" alt="Степан Васильев">
+                        <p class="mt30 text-center"><a href="/shop" class="button-more addtocartbutton">Посмотреть
+                                другие каффы</a>
+                        </p>
                     </div>
-
-                    <div class="col-sm-4">
-                        <img src="/images/bag.jpg">
-                        <p>Подарочный пакет</p>
+                    <div class="col-lg-4 d-none d-lg-flex">
+                        <p class="stepa-abouts">Каффы - это настоящая находка для тех, у кого не проколоты ушки, ведь
+                            большинство
+                            моделей не требуют проколов вообще. Попробуйте и вы кое-что особенное...</p>
                     </div>
                 </div>
             </div>
 
-        </div>
-    </div>
-</div>
+            <!-- Initialize Swiper -->
+            <script>
+                var swiper = new Swiper('.swiper-container', {
+                    pagination: '.swiper-pagination',
+                    nextButton: '.swiper-button-next',
+                    prevButton: '.swiper-button-prev',
+                    slidesPerView: 1,
+                    paginationClickable: true,
+                    spaceBetween: 30,
+                    loop: true
+                });
+            </script>
 
 
-<div class="main-content-area">
+            <div class="block-1400">
+                <div class="bg-grey pdb30">
+                    <div class="pd30 text-center komplect">
+                        <div class="container">
+                            <hr>
+                        </div>
+                        <h2>Комплект поставки</h2>
+                        <div class="container ">
+
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <img src="/images/box.jpg">
+                                    <p>Ювелирная коробочка</p>
+                                </div>
+                                <div class="col-sm-4">
+                                    <?php echo get_the_post_thumbnail($page->ID, 'full'); ?>
+                                    <p>Кафф <?php $key = "short_name";
+                                        echo get_post_meta($post->ID, $key, true); ?></p>
+                                </div>
+
+                                <div class="col-sm-4">
+                                    <img src="/images/bag.jpg">
+                                    <p>Подарочный пакет</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="main-content-area">
